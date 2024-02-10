48972c2fdbec17478759984ca9e884f55f9269e4
Safin Ali <safin.ali.7205@gmail.com>
2024-02-10 09:24:24 +0600

    refactor: :heavy_plus_sign: nextui library wrapped in project

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Header_Nav from '@/components/AppBar/Header_Nav';
+
+
+export default function Home() {
+	return (
+		<>
+			<Header_Nav/>
+
+		</>
+	);
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,28 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/global.css";
+import { Providers } from './_personal/providers';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "MOVIES4U",
-  description: "Home Page Of Movies4U",
+	title: "MOVIES4U",
+	description: "Home Page Of Movies4U",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={ inter.className }>
+				<Providers>
+					{ children }
+				</Providers>
+
+			</body>
+		</html>
+	);
 }
