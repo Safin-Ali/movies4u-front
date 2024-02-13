8385a1ac2b21b745a6292758dcc1f71684111d2f
Safin Ali <safin.ali.7205@gmail.com>
2024-02-14 00:49:02 +0600

    feat: :art: a simple home page without mobile responsive
    a new Category_Block component for fetch and render movies data as Movie_Thumb Card. and implemented in root page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,22 @@
-import Header_Nav from '@/components/AppBar/Header_Nav';
+import Header_Nav from '@/components/CSR/AppBar/Header_Nav';
+import Category_Block from '@/components/SSR/Section/Category_Block';
 
 
 export default function Home() {
 	return (
 		<>
 			<Header_Nav/>
+			<main>
+				{/* Trending Movie */}
 
+				<Category_Block content_header={'Trending'} api={'movies/?q=trending/movie/day?language=en-US'}/>
+
+				<Category_Block content_header={'Popular'} api={'movies/?q=movie/popular'}/>
+
+				<Category_Block content_header={'Top Rated'} api={'movies/?q=movie/top_rated'}/>
+
+				<Category_Block content_header={'Now Playing'} api={'movies/?q=movie/now_playing'}/>
+			</main>
 		</>
 	);
 }
---
+++ src/components/SSR/Section/Category_Block.tsx
@@ -0,0 +1,56 @@
+import { moviesDBApi } from '@/services/http-fetch-ins';
+import React from 'react';
+import Movie_Thumb from '../Card/Movie_Thumb';
+import { Button } from '@nextui-org/react';
+import { IoIosArrowForward } from "react-icons/io";
+
+interface Props {
+	content_header: string,
+	api: string,
+}
+
+export default async function Category_Block(props: Props) {
+
+	const { results } = await (await moviesDBApi.get(props.api)).json();
+
+	return (
+		<section className={ `px-6 my-5` }>
+			<div className={ `my-5` }>
+				<div className={`flex justify-between items-center`}>
+					<div>
+						<h3 className={ `text-xl font-semibold` }>
+							{ props.content_header }
+						</h3>
+					</div>
+
+					<div>
+						<Button
+							variant={'light'}
+							className={`hover:bg-default-900/10`}
+							radius={'sm'}
+							endContent={<IoIosArrowForward/>}
+						>
+							SEE ALL
+						</Button>
+					</div>
+				</div>
+			</div>
+
+			<div className={ `g-full-center gap-5 grid-cols-5` }>
+				{
+					results.slice(0, 5).map((dt: any) => {
+						return (
+							<Movie_Thumb
+								key={ dt.id }
+								src={ `https://image.tmdb.org/t/p/original/${dt.poster_path}` }
+								genre={ dt.genre_ids.slice(0, 2) }
+								rating={ dt.vote_average }
+								title={ dt.title }
+							/>
+						)
+					})
+				}
+			</div>
+		</section>
+	);
+}
